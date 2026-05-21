fe50912c07141ce388d46acf6aded5887c32e0fc
Darly <darly@ufu.br>
2026-05-21 11:28:44 -0300

    sync: 21/05/2026, 11:28:44

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://editorapoisson.com.br',
+  integrations: [react(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
