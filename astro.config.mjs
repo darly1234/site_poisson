ae852728b49f26a617840c388bc1b22a8c7ff2e5
Darly <darly@ufu.br>
2026-05-22 17:11:29 -0300

    sync: 22/05/2026, 17:11:28

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://editorapoisson.com.br',
+  redirects: {
+    '/chamadas': '/chamadas-abertas'
+  },
   integrations: [react(), sitemap()],
 
   vite: {
